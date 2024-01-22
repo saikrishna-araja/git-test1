3ae03467957868b5ca5fa45ff571e13323f2802c
Sai krishna Araja <saraja@uncc.edu>
2024-01-22 03:08:26 -0500

    files created for assignment2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # git-test1
 NBAD-assignment02
+
+Server URL -137.184.208.227
---
+++ uncc.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Test Page</title>
+</head>
+<body>
+    <h1>Saikrishna's first PHP program</h1>
+
+
+    <h2><?php echo 9*13; ?></h2>
+</body>
+</html>
